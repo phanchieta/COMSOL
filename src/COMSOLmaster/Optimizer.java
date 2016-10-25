5f84cc367e1f4187b0067ca89ab5e94878491172
pchagas <pchagas@CCEE-GDT-088.ce.ncsu.edu>
2016-10-25 13:10:24 -0400

    Working with baffled reactor

--- src/COMSOLmaster/Optimizer.java
+++ src/COMSOLmaster/Optimizer.java
@@ -39,12 +39,28 @@ import org.jenetics.util.DoubleRange;
 public class Optimizer {
         
         private static String classfile;
-        private static final double RECT1_Y_MIN = 0;
-        private static final double RECT1_Y_MAX = 1;
-        private static final double RECT2_SIZE_Y_MIN = 0.5;
-        private static final double RECT2_SIZE_Y_MAX = 10;
+        private static final double BAFFLE_MIN = 0;
+        private static final double BAFFLE_MAX = 0.66; 
+        private static final double RECT2_Y_MIN = 0;
+        private static final double RECT2_Y_MAX = 1;
+        private static final double RECT1_SIZE_Y_MIN = 0.5;
+        private static final double RECT1_SIZE_Y_MAX = 10;
         private static final double RECT3_Y_MIN = 0;
         private static final double RECT3_Y_MAX = 1;
+        private static final double RECT4_Y_MIN = 0.124;
+        private static final double RECT4_Y_MAX = 0.2;
+        private static final double RECT5_Y_MIN = 0.34;
+        private static final double RECT5_Y_MAX = 0.448;
+        private static final double RECT6_Y_MIN = 0.6;
+        private static final double RECT6_Y_MAX = 0.7;
+        private static final double RECT7_Y_MIN = 0.23;
+        private static final double RECT7_Y_MAX = 0.3;
+        private static final double RECT8_Y_MIN = 0.48;
+        private static final double RECT8_Y_MAX = 0.55;
+        private static final double RECT9_Y_MIN = 0.74;
+        private static final double RECT9_Y_MAX = 0.8;
+ 
+        
         private final double mutationProb; //0.03
         private final double xOver; //0.6
         private final int nThreads;
@@ -78,14 +94,28 @@ public class Optimizer {
                 if (line == null) { break; }
                 System.out.println(line);
             }
-            double rect2X, rect1Ypos, rect3Ypos;
+            /*double rect2X, rect1Ypos, rect3Ypos;
             
             rect1Ypos = rect3Ypos = x[1] - 0.1;
             rect1Ypos = rect1Ypos * x[0];
             rect3Ypos = rect3Ypos * x[2];
             rect2X = 5/x[1];
             
-            builder.command("cmd.exe", "/c", "comsolbatch -inputfile " + classfile + " \"%cd%\" " + rect1Ypos + " " + rect2X + " " + x[1] + " " + rect3Ypos);
+            builder.command("cmd.exe", "/c", "comsolbatch -inputfile " + classfile + " \"%cd%\" " + rect1Ypos + " " + rect2X + " " + x[1] + " " + rect3Ypos);*/
+            
+            
+            double rect1X, rect2Ypos, rect3Ypos;
+            
+            rect2Ypos = rect3Ypos = x[0] - 0.12;
+            rect2Ypos = rect2Ypos * x[1];
+            rect3Ypos = rect3Ypos * x[2];
+            rect1X = 5/x[0];
+                    
+            builder.command("cmd.exe", "/c", "comsolbatch -inputfile " + classfile + " \"%cd%\" " + rect1X + " " + x[0] + " " + rect2Ypos + " " + rect3Ypos
+            + " " + x[3]*rect1X+ " " + x[4]*x[0]+ " " + x[5]*rect1X+ " " + x[6]*x[0]+ " " + x[7]*rect1X + " " + x[8]*x[0] + " " + x[9]*rect1X+ " " + x[10]*x[0] + " " + x[11]*rect1X+ " " + x[12]*x[0] + " " + x[13]*rect1X+ " " + x[14]*x[0]);
+                    
+                    
+                    
             p = builder.start();
             r = new BufferedReader(new InputStreamReader(p.getInputStream()));
             
@@ -122,7 +152,8 @@ public class Optimizer {
                     
             PrintWriter out = new PrintWriter(bw))
             {
-                out.println(rect1Ypos + " " + rect2X + " " + x[1] + " " + rect3Ypos + " ->" + or.avgSpeed()); /*Printing each fitness function to file conv.txt*/
+                out.println(rect1X + " " + x[0] + " " + rect2Ypos + " " + rect3Ypos
+            + " " + x[3]*rect1X+ " " + x[4]*x[0]+ " " + x[5]*rect1X+ " " + x[6]*x[0]+ " " + x[7]*rect1X + " " + x[8]*x[0] + " " + x[9]*rect1X+ " " + x[10]*x[0] + " " + x[11]*rect1X+ " " + x[12]*x[0] + " " + x[13]*rect1X+ " " + x[14]*x[0] + " ->" + or.avgSpeed()); /*Printing each fitness function to file conv.txt*/
             } 
             catch (IOException e) 
             {
@@ -149,7 +180,15 @@ public class Optimizer {
         final Engine<DoubleGene, Double> engine = Engine
 			.builder(
 				Optimizer::fitness,
-				codecs.ofVector(DoubleRange.of(RECT1_Y_MIN,RECT1_Y_MAX),DoubleRange.of(RECT2_SIZE_Y_MIN,RECT2_SIZE_Y_MAX),DoubleRange.of(RECT3_Y_MIN,RECT3_Y_MAX)))
+				codecs.ofVector(DoubleRange.of(RECT1_SIZE_Y_MIN,RECT1_SIZE_Y_MAX),
+                                        DoubleRange.of(RECT2_Y_MIN,RECT2_Y_MAX),
+                                        DoubleRange.of(RECT3_Y_MIN,RECT3_Y_MAX),
+                                        DoubleRange.of(BAFFLE_MIN,BAFFLE_MAX),DoubleRange.of(RECT4_Y_MIN,RECT4_Y_MAX),
+                                        DoubleRange.of(BAFFLE_MIN,BAFFLE_MAX),DoubleRange.of(RECT5_Y_MIN,RECT5_Y_MAX),
+                                        DoubleRange.of(BAFFLE_MIN,BAFFLE_MAX),DoubleRange.of(RECT6_Y_MIN,RECT6_Y_MAX),
+                                        DoubleRange.of(BAFFLE_MIN,BAFFLE_MAX),DoubleRange.of(RECT7_Y_MIN,RECT7_Y_MAX),
+                                        DoubleRange.of(BAFFLE_MIN,BAFFLE_MAX),DoubleRange.of(RECT8_Y_MIN,RECT8_Y_MAX),
+                                        DoubleRange.of(BAFFLE_MIN,BAFFLE_MAX),DoubleRange.of(RECT9_Y_MIN,RECT9_Y_MAX)))
                         .populationSize(popSize)
                         .offspringFraction(0.5)
                         .optimize(Optimize.MAXIMUM)
@@ -168,8 +207,8 @@ public class Optimizer {
 			.collect(toBestPhenotype());*/
                 
                 final EvolutionResult<DoubleGene, Double> best = engine.stream() 
-                        .limit(bySteadyFitness (7))
-                        .limit(300)
+                        .limit(bySteadyFitness (5))
+                        .limit(100)
                         .peek(statistics)
                         .collect(EvolutionResult.toBestEvolutionResult()); /*Forcing elitism*/
                 
